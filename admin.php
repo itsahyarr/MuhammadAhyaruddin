8516605e86de97521c838f6abb055533c7fb2300
itsahyarr <ahyaruddin07@gmail.com>
2020-12-16 14:06:06 +0700

    first push login form

---
+++ admin.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Admin</title>
+</head>
+<body>
+	<h1>Selamat Datang ADMIN!</h1>	
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Login</title>
+</head>
+<body>
+	<center>
+		<h1>Form Login</h1>
+		<form action="admin.php">
+			<table>
+				<tr>
+					<td><label>Username :</label></td>
+					<td><input type="text" name="username"></td>
+				</tr>
+				<tr>
+					<td><label>Password :</label></td>
+					<td><input type="password" name="password"></td>
+				</tr>
+				<tr>
+					<td colspan="2"><button type="submit" style="float: right;">Login</button></td>
+				</tr>
+			</table>
+		</form>
+	</center>
+</body>
+</html>
